6337c52520fc741cb02258a75c9daeae6a3d63a7
danad1821 <danadabdoub@gmail.com>
2024-10-21 20:42:59 +0300

    added comments to the dafsa class

--- dafsa.js
+++ dafsa.js
@@ -7,19 +7,18 @@ class DAFSA {
     this.accepts_empty_string = false;
     this.heights = {};
   }
-
+  //adds an initial state
   add_initial_state(state) {
     if (this.initial_state === null) {
       this.initial_state = state;
-      this.states[state] = [];
-
-      if (this.accepts_empty_string) {
+      //if the machine doesn't accept empty strings it will add it to the non final states
+      //otherwise it will add it to the final states array
+      if (this.accepts_empty_string == false) {
         this.add_non_final_state(this.initial_state);
       } else {
         this.add_final_state(this.initial_state);
       }
-
-      return true;
+      return true; //state is added
     } else {
       return false; // state already exists
     }
@@ -30,9 +29,9 @@ class DAFSA {
       !this.states.hasOwnProperty(state) &&
       !this.final_states.includes(state)
     ) {
-      this.final_states.push(state);
-      this.states[state] = []; // Initialize empty list for adjacent states
-      return true;
+      this.final_states.push(state); // adds the state to the final states
+      this.states[state] = []; // Initialize empty list for the states it goes to
+      return true; //state is added
     } else {
       return false; // state already exists
     }
@@ -43,20 +42,23 @@ class DAFSA {
       !this.non_final_states.includes(state) &&
       !this.states.hasOwnProperty(state)
     ) {
-      this.non_final_states.push(state);
-      this.states[state] = []; // Initialize empty list for adjacent states
-      return true;
+      this.non_final_states.push(state); //adds the state to the non final states
+      this.states[state] = []; // Initialize empty list for the states it goes to
+      return true; //state is added
     } else {
       return false; // state already exists
     }
   }
 
   non_final_to_final_conversion(state) {
-    this.non_final_states = this.non_final_states.filter((s) => s !== state);
-    this.final_states.push(state);
+    // converts no final states into final states
+    // this is needed if the user for example added the string 'aa' before string 'a' and they are both accepted 
+    this.non_final_states = this.non_final_states.filter((s) => s !== state); //removes the state from the non final states
+    this.final_states.push(state); // adds it to the final
   }
 
   add_edge(v1, v2, sym) {
+    //adds a connects between 2 states by adding an array to the array for the state's key in the this.states object
     if (!this.states.hasOwnProperty(v1) || !this.states.hasOwnProperty(v2)) {
       return false; // Either state doesn't exist
     }
@@ -68,96 +70,95 @@ class DAFSA {
     }
 
     const added_edge = [v2, sym];
-    this.states[v1].push(added_edge);
-    return true;
+    this.states[v1].push(added_edge); // adds array of state and symbol seen to get to it
+    return true; // edge has been added
   }
 
   isAccepted(s) {
+    //checks if a string is accepted by the machine
     let currentState = this.initial_state;
 
-    for (const character of s) {
+    for (const character of s) { // goes the characters of the string being searched for
       if (this.states[currentState].length === 0) {
+        //if there are more characters but the current state doesn't have any edges 
         return false;
       }
 
       let changeOccurred = false;
       for (const v of this.states[currentState]) {
-        if (v[1] === character) {
-          currentState = v[0];
-          changeOccurred = true;
+        if (v[1] === character) { // sees if there is a transition on the current character
+          currentState = v[0]; // changes the current state to the state it reached 
+          changeOccurred = true; // changes the value of changeOccured since a change occured
           break;
         }
       }
-
+      //if no change occured that means the string is not in the list of accepted strings
       if (!changeOccurred) {
         return false;
       }
     }
-
+    // if the state it is currently at is in the list of final states then the string is accepted
     return this.final_states.includes(currentState);
   }
 
   add_accepted_string(s) {
     const slen = s.length;
-
-    if (this.initial_state === null) {
+    if (this.initial_state === null) { // if it is the first string to be accepted by the machine
+      // adds an initial state
       this.add_initial_state("-1, 0");
-
+      //adds states until before it the last character is reached
       for (let i = 0; i < slen; i++) {
-        if (i + 1 !== slen) {
+        if (i + 1 !== slen) { // checks to make sure what is being added is not the final state
           this.add_non_final_state("-1, " + (i + 1));
           this.add_edge("-1, " + i, "-1, " + (i + 1), s[i]);
         }
       }
-
+      //adds final state and the the edge to get to it
       this.add_final_state("-1, " + slen);
       this.add_edge("-1, " + (slen - 1), "-1, " + slen, s[slen - 1]);
-      this.last_added_state = slen;
-    } else {
+    } else { // there already is one accepted string
       let currentState = this.initial_state;
-      const n = Object.keys(this.states).length;
-      let ind = 0;
+      let ind = 0; // used to know where to start adding more states from
 
       for (let index = 0; index < s.length; index++) {
         let changeOccurred = false;
 
         for (const v of this.states[currentState]) {
           if (v[1] === s[index]) {
-            currentState = v[0];
-            ind = index + 1;
-            changeOccurred = true;
+            currentState = v[0]; //changes the current state to the one reached by the machine
+            ind = index + 1; // add 1 to ind to later determine where to start adding states
+            changeOccurred = true;// changes the value of changeOccured since a change occured
             break;
           }
         }
-
+        //if no change occured that means it should start adding states after this one
         if (!changeOccurred) {
           break;
         }
       }
-
+      // the state already exists and might just need to be converted into a final state
       if (ind === slen) {
-        this.non_final_to_final_conversion(currentState);
+        this.non_final_to_final_conversion(currentState); // converts from non final to final state
         return;
       }
-
-      for (let index = 0; index < s.length; index++) {
-        this.heights[index] = [];
-
+      const n = Object.keys(this.states).length; // get the number of states in the machine
+      //
+      for (let index = 0; index < slen; index++) {
         if (index >= ind && index !== slen - 1) {
           this.add_non_final_state("-1, " + n);
           this.add_edge(currentState, "-1, " + n, s[index]);
-          currentState = "-1, " + n;
-          n++;
+          currentState = "-1, " + n; //changes current state to the state just creates
+          n++; // adds 1 to n
         }
       }
-
+      // adds the final state used to accept this new string
       this.add_final_state("-1, " + n);
       this.add_edge(currentState, "-1, " + n, s[slen - 1]);
-      this.heights[slen] = [];
     }
   }
 
   get_states() {
+    //returns the object containing all the information about all states
     return this.states;
   }
 
