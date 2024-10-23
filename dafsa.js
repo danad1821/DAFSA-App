f65a2ad83d625d36f786dcb3c1caedd3dbc153c8
danad1821 <danadabdoub@gmail.com>
2024-10-23 21:54:18 +0300

    general creation of a diagram

--- dafsa.js
+++ dafsa.js
@@ -1,25 +1,5 @@
 let machine = document.getElementById("machine");
 
-function addState(state, isFinal) {
-  let st = document.createElement("div");
-  let stClasses = st.classList;
-  stClasses.add("state");
-  st.textContent=state;
-  st.setAttribute('id', state.toString());
-  if(isFinal){
-    stClasses.add('finalState')
-  }
-  machine.appendChild(st)
-}
-
-function addEdge(state1, state2, sym){
-  new LeaderLine(
-    document.getElementById(state1),
-    document.getElementById(state2),
-    {endLabel: LeaderLine.pathLabel(sym)}
-  );
-}
-
 class DAFSA {
   constructor() {
     this.states = {};
@@ -53,7 +33,6 @@ class DAFSA {
     ) {
       this.final_states.push(state); // adds the state to the final states
       this.states[state] = []; // Initialize empty list for the states it goes to
-      addState(state, true);
       return true; //state is added
     } else {
       return false; // state already exists
@@ -67,7 +46,6 @@ class DAFSA {
     ) {
       this.non_final_states.push(state); //adds the state to the non final states
       this.states[state] = []; // Initialize empty list for the states it goes to
-      addState(state, false)
       return true; //state is added
     } else {
       return false; // state already exists
@@ -95,7 +73,6 @@ class DAFSA {
 
     const added_edge = [v2, sym];
     this.states[v1].push(added_edge); // adds array of state and symbol seen to get to it
-    addEdge(v1, v2, sym);
     return true; // edge has been added
   }
 
@@ -190,6 +167,109 @@ class DAFSA {
   get_states() {
     //returns the object containing all the information about all states
     return this.states;
+  }
+
+  createDirectedGraph() {
+    machine.innerHTML="";
+    let nodes = [];
+    let links = [];
+    let symbols=[]
+    for (const v of Object.keys(this.states)) {
+      nodes.push({ id: v.toString() });
+      for (const edge of this.states[v]) {
+        links.push({ source: v.toString(), target: edge[0].toString() });
+        symbols.push(edge[1])
+      }
+    }
+
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
+    const width = 928;
+    const height = 600;
+    const simulation = d3
+      .forceSimulation(nodes)
+      .force(
+        "link",
+        d3.forceLink(links).id((d) => d.id)
+      )
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(width / 2, height / 2))
+      .on("tick", ticked);
+
+    // Create the SVG container.
+    const svg = d3
+      .create("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [0, 0, width, height])
+      .attr("style", "max-width: 100%; height: auto;");
+
+    // Add a line for each link, and a circle for each node.
+    const link = svg
+      .append("g")
+      .attr("stroke", "#999")
+      .attr("stroke-opacity", 0.6)
+      .selectAll()
+      .data(links)
+      .join("line")
+      .attr("stroke-width", (d) => Math.sqrt(d.value));
+
+    const node = svg
+      .append("g")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 3)
+      .selectAll()
+      .data(nodes)
+      .join("circle")
+      .attr("r", 10)
+      .attr("fill", (d) => color(d.group));
+
+    node.append("title").text((d) => d.id);
+
+    // Add a drag behavior.
+    node.call(
+      d3
+        .drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended)
+    );
+
+    // Set the position attributes of links and nodes each time the simulation ticks.
+    function ticked() {
+      link
+        .attr("x1", (d) => d.source.x)
+        .attr("y1", (d) => d.source.y)
+        .attr("x2", (d) => d.target.x)
+        .attr("y2", (d) => d.target.y);
+
+      node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+    }
+
+    // Reheat the simulation when drag starts, and fix the subject position.
+    function dragstarted(event) {
+      if (!event.active) simulation.alphaTarget(0.3).restart();
+      event.subject.fx = event.subject.x;
+      event.subject.fy = event.subject.y;
+    }
+
+    // Update the subject (dragged node) position during drag.
+    function dragged(event) {
+      event.subject.fx = event.x;
+      event.subject.fy = event.y;
+    }
+
+    // Restore the target alpha so the simulation cools after dragging ends.
+    // Unfix the subject position now that it’s no longer being dragged.
+    function dragended(event) {
+      if (!event.active) simulation.alphaTarget(0);
+      event.subject.fx = null;
+      event.subject.fy = null;
+    }
+    
+    let sv = document.createElement("svg");
+    sv = svg.node();
+    return svg.node()
+    
   }
 
   minimize_dafsa(D) {
--- main.js
+++ main.js
@@ -5,6 +5,7 @@ addBtn.addEventListener("click", function(){
   let acceptedString=document.getElementById('acceptedString');
   D.add_accepted_string(acceptedString.value);
   acceptedString.value="";
+  machine.appendChild(D.createDirectedGraph());
 })
 
 let searchBtn=document.getElementById('searchBtn');
