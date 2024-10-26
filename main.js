d4309cfc1ab0d61f95442e5d848f4e9e4a4632a7
KarinAlZoabi <karinalzoabi15@gmail.com>
2024-10-26 22:01:11 +0300

    added the download image option

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let D = new DAFSA();
 let addBtn = document.getElementById('addBtn');
 let acceptedString = document.getElementById('acceptedString');
 let resetBtn = document.getElementById('resetBtn');
-let emptyStringCheckBox=document.getElementById('emptyCheckbox')
+let emptyStringCheckBox = document.getElementById('emptyCheckbox')
 
 function createMachine() { //function to create DAFSA machine
   D.add_accepted_string(acceptedString.value);
@@ -36,19 +36,36 @@ function resetMachine() {
 
 resetBtn.addEventListener("click", resetMachine);
 
-emptyStringCheckBox.addEventListener('change', ()=>{
+emptyStringCheckBox.addEventListener('change', () => {
   D.acceptance_of_empty_string(emptyStringCheckBox.checked)
-  if(emptyStringCheckBox.checked==true || Object.keys(D.states).length>1){
+  if (emptyStringCheckBox.checked == true || Object.keys(D.states).length > 1) {
     machine.appendChild(D.createDirectedGraph());
   }
-  else{
-    machine.innerHTML="";
+  else {
+    machine.innerHTML = "";
   }
 })
 
-let removeBtn=document.getElementById('removeBtn');
-removeBtn.addEventListener('click', ()=>{
-  let removeText=document.getElementById('removeText');
+let removeBtn = document.getElementById('removeBtn');
+removeBtn.addEventListener('click', () => {
+  let removeText = document.getElementById('removeText');
   console.log(D.remove_accepted_string(removeText.value));
   machine.appendChild(D.createDirectedGraph());
 })
+
+function downloadGraphAsImage() {
+  const svgElement = document.querySelector("#machine");
+  const format = document.getElementById("format").value;
+
+  html2canvas(svgElement, {
+    useCORS: true,    // Enables cross-origin images if any are used
+    scale: 2,         // Increases the resolution of the downloaded image
+    backgroundColor: "#ffffff" // Sets a white background
+  }).then(canvas => {
+    // Create a download link
+    const link = document.createElement("a");
+    link.download = `DAFSA-machine-image.${format}`;
+    link.href = canvas.toDataURL(`image/${format}`);
+    link.click();
+  });
+}
